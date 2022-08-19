4f50e84be4b9cfaa650da1c60b9a4d96230448f3
Sathish Kumar <85840414+SatizRamamoorthy@users.noreply.github.com>
2022-08-19 17:32:15 +0530

    Add files via upload

---
+++ com.covid/src/main/java/com/covid/CovidCases.java
@@ -0,0 +1,21 @@
+package com.covid;
+
+public class CovidCases {
+	
+	static int sum = 0;
+
+	public static void main(String[] args) {
+		
+		System.out.println(sumOfCovidCases(1,2));
+			
+		}
+	
+	public static int sumOfCovidCases (int a, int b) {
+		
+		 sum = a + b;
+		
+		return sum;
+
+	}
+
+}
